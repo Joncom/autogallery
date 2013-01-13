69cbb307ee30ee3f71e6ea7014d21a78a17e1455
Jonathan Commins <joncom@gmail.com>
2013-01-12 17:10:00 -0800

    Removed script that does not appear to add anything.

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
 
 		<script type="text/javascript" src="js/jquery-1.7.2.min.js"></script>
 		<script type="text/javascript" src="js/jquery-ui-1.8.18.custom.min.js"></script>
-		<script type="text/javascript" src="js/jquery.smooth-scroll.min.js"></script>
 		<script type="text/javascript" src="js/lightbox.js"></script>
 
 		<link href="css/reset.css" rel="stylesheet" />
