fca5edb239c012492076df7b4fccb344307544bb
Jonathan Commins <joncom@gmail.com>
2013-01-12 16:56:57 -0800

    Stripped gallery down essential parts.

--- index.php
+++ index.php
@@ -1,41 +1,26 @@
-<?php
+<html>
 
-$gallery_dir = './images/gal/';
+	<head>
 
-?>
+		<script type="text/javascript" src="js/jquery-1.7.2.min.js"></script>
+		<script type="text/javascript" src="js/jquery-ui-1.8.18.custom.min.js"></script>
+		<script type="text/javascript" src="js/jquery.smooth-scroll.min.js"></script>
+		<script type="text/javascript" src="js/lightbox.js"></script>
 
-<div id="photos">
+	</head>
 
-	<?php for( $i = 0; $i < count($photos); $i++ ) { ?><div
+	<body>
 
-		class="thumb-box"><a
+		<div id="photos">
 
-			href="<?php echo $gallery_dir . $photos[$i]['img']; ?>"
+			<?php for( $i = 1; $i <= 3; $i++ ) { ?>
 
-			rel="lightbox[<?php echo $photos[$i]['type']; ?>]"
+				<a href="images/<?php echo $i; ?>.jpg" rel="lightbox" title="my caption">image #<?php echo $i; ?></a>
 
-			aTitle="<?php echo htmlspecialchars($photos[$i]['title']); ?>"><div
+			<?php } ?>
 
-				id="thumb-text-<?php echo $i; ?>"
+		</div>
 
-				class="thumb-text"><?php echo nl2br($photos[$i]['info']); ?></div><div
+	</body>
 
-					class="hitbox"
-
-					onmouseover="$('#thumb-text-<?php echo $i; ?>').show(0);"
-
-					onmouseout="$('#thumb-text-<?php echo $i; ?>').hide(0);"></div><img
-
-						<?php if( ($i+1) % 5 === 0 ) { ?>class="last-in-row"<?php } ?>
-
-						src="<?php if($photos[$i]['status']==1) { echo './sold-stamp.php?'; } echo $gallery_dir . $photos[$i]['img_thumb']; ?>"
-
-						alt="<?php echo ucfirst($photos[$i]['type']); ?>: image <?php echo ($i + 1); ?> of <?php count($photos); ?> thumb" /></a></div><?php } ?>
-
-</div>
-
-<!-- Scripts -->
-<script type="text/javascript" src="js/jquery-1.7.2.min.js"></script>
-<script type="text/javascript" src="js/jquery-ui-1.8.18.custom.min.js"></script>
-<script type="text/javascript" src="js/jquery.smooth-scroll.min.js"></script>
-<script type="text/javascript" src="js/lightbox.js"></script>
+</html>
